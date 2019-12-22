a964b2b808b25d102d3721fd903d52ec9fd54fbf
Jean Michel <jean.engenheirocomp@gmail.com>
2019-12-22 12:59:01 -0300

    order and order item model

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -4,6 +4,22 @@
 const Model = use('Model')
 
 class Order extends Model {
+
+    items() {
+        return this.hasMany('App/Models/OrderItem')
+    }
+
+    coupons() {
+        return this.belongsToMany('App/Models/Coupon')
+    }
+
+    discounts() {
+        return this.hasMany('App/Models/Discount')
+    }
+
+    user() {
+        return this.belongsTo('App/Models/User', 'user_id', 'id')
+    }
 }
 
 module.exports = Order
