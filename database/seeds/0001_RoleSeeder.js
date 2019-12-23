6ff96e71b6fbe067d13a32d24048bb6f2f51de68
Jean Michel <jean.engenheirocomp@gmail.com>
2019-12-23 09:55:49 -0300

    Fix create method name on line 31

--- database/seeds/0001_RoleSeeder.js
+++ database/seeds/0001_RoleSeeder.js
@@ -15,20 +15,20 @@ const Role = use('Role')
 class RoleSeeder {
   async run () {
     // Define o cargo de administrador do sistema
-    await new Role.create({
+    await Role.create({
       name: 'Admin',
       slug: 'admin',
       description: 'System Administrator'
     })
     // Define o cargo de gerente da loja
-    await new Role.create({
+    await Role.create({
       name: 'Manager',
       slug: 'manager',
       description: 'Store manager'
     })
 
     // Define o cargo de cliente da loja
-    await new Role.create({
+    await Role.create({
       name: 'Customer',
       slug: 'customer',
       description: 'Store customer'
